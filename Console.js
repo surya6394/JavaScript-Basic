859c90763f7ca8388c38d9942c7b179dd69e33ca
surya6394 <ravinigam0720@gmail.com>
2023-10-18 00:57:15 +0530

    console with datatypes and arithmatic operator

---
+++ Console.js
@@ -0,0 +1,99 @@
+                                        
+console.log("Hello world");
+
+console.log(8);
+
+console.log(true);
+
+
+                                        // Addition ( + )
+                            
+console.log("Addition")
+
+console.log(8+10);
+
+console.log("11"+8); // String priority
+
+console.log(9+"11"); // string priority
+
+console.log(9+true); // taken true = 1
+
+console.log(true+true) // taken true = 1 and addition=2
+
+console.log(false+6) // taken false = 0
+
+console.log(false+false) // taken false = 0 and addition = 0
+
+console.log(true+"11") // output = true11
+
+console.log("first"+true) // String priority ( boolean converted into string)
+
+console.log(true+false) // true = 1 & flase = 0 ==> 1
+
+
+                                        // Substraction ( - )
+                    
+console.log("Substraction")
+
+console.log(12-7)
+
+console.log(15-2.3)
+
+console.log("11"-5) // output = 6 (Integer priority)
+
+console.log("11"-"4") // opt = 6 ( String converted into integer )
+
+console.log("11"-true) // String converted into integer and true = 1
+
+console.log(true-false) // Integer priority
+
+console.log(12-"3") // String convert into integer
+
+
+
+                                        // Multiplication
+
+console.log("Multiplication")
+                                    
+console.log(8*9)
+
+console.log(8*2.5)
+
+console.log("11"*5) // Integer priority ( String converted into integer)
+
+console.log("11"*"3") // Integer priority opt=33
+
+console.log(8*true) // Integer priority opt= 8
+
+console.log(true*9) // Integer priority
+
+console.log("7"*true) // Both converted into integer
+
+console.log(true*false) // Both converted into integer
+
+
+
+                                        // Division
+
+console.log("Division")
+
+console.log(9/3)
+
+console.log(9/3.2)
+
+console.log("11"/3) // Integer priority
+
+console.log("12"/"3") // integer priotity
+
+console.log("15"/true) // integer priority
+
+console.log("12"/false) // opt = infinity
+
+console.log(true/false) // integer priority
+
+
+
+
+
+
+
