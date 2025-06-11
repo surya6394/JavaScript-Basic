f5bc07cb6d5fb50b16cb11013c016ce99a62229e
SuryaPratap-ClevDoc <suryapratap@clevdoc.com>
2025-06-11 18:44:35 +0530

    Strictly equals, Looseequals done

---
+++ Strictly-loose-equality/script.js
@@ -0,0 +1,18 @@
+
+console.log(null === undefined); 
+console.log(null == undefined); 
+
+console.log(0 == "0" == 0);
+
+console.log(0 == null);
+console.log(0 == false);
+console.log(0 === false);
+
+console.log(NaN == NaN);
+console.log(NaN === NaN)
+
+console.log([] == [])
+// console.log([] === []);
+
+console.log({} == {});
+// console.log({} === {});
